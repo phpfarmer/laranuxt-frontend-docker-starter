50ea3a601fee6b20695e87646d5af81802d730af
Juyal Ahmed <tojibon@gmail.com>
2024-09-03 21:02:33 +0200

    LAR-13 - Profile information update API call optimized and single composables useFormHandler

--- pages/profile/partials/UpdateProfileInformationForm.vue
+++ pages/profile/partials/UpdateProfileInformationForm.vue
@@ -1,30 +1,17 @@
 <script setup>
-import {InputLabel, PrimaryButton, TextInput} from '~/components/UI/index.js';
-import {computed, ref} from "vue";
-import { useVuelidate } from '@vuelidate/core';
 import { required, email } from '@vuelidate/validators';
-import {useAuthStore} from "~/stores/auth";
-import {AlartErrorMessage, AlartSuccessMessage, FormTooManyAttempt, FormHeader} from '~/components/Form/index.js';
-import {useRuntimeConfig, useRouter} from "nuxt/app";
-import ContentBox from "~/components/UI/ContentBox.vue";
+import { useAuthStore } from '~/stores/auth';
+import { useRuntimeConfig, useRouter } from 'nuxt/app';
+import { FormTooManyAttempt, AlartErrorMessage, AlartSuccessMessage } from '~/components/Form/index.js';
+import { useFormHandler } from '~/composables/useFormHandler';
+import {InputLabel, PrimaryButton, TextInput, ContentBox} from "~/components/UI/index.js";
 
 const props = defineProps({
   value: { type: Object, default: () => ({ id: '', name: '', email: '', email_update: null }) },
 });
 
-const router = useRouter()
-
-const loading = ref(false)
-const success = ref(false)
-const error = ref(false);
-const message = ref('');
-const submitCount = ref(0);
-
-const auth = useAuthStore()
-
-const isTooManyAttempts = computed(() => {
-  return submitCount.value >= 5
-});
+const router = useRouter();
+const auth = useAuthStore();
 
 const rules = {
   value: {
@@ -33,154 +20,71 @@ const rules = {
   },
 };
 
-const v$ = useVuelidate(rules, props);
-const isSubmitDisabled = computed(() => {
-  return v$.value.$invalid;
+const { v$, loading, success, error, message, isSubmitDisabled, isTooManyAttempts, onSubmit, apiRequest } = useFormHandler(rules, props, {
+  url: `${useRuntimeConfig().public.apiBaseUrl}/api/account/profile`,
+  method: 'POST',
 });
 
-const onSubmit = async () => {
-  v$.value.$touch();
-  if (isSubmitDisabled.value) {
-    return false;
-  }
-
-  const config = useRuntimeConfig();
-  success.value = false;
-  error.value = false;
-  message.value = null;
-  loading.value = true;
+const handleSubmitProfileInformation = async () => {
+  if (isSubmitDisabled.value) return;
 
   try {
-    const response = await fetch(`${config.public.apiBaseUrl}/api/account/profile`, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      credentials: 'include',
-      body: JSON.stringify({
-        name: props.value.name,
-        email: props.value.email,
-      }),
-    });
-
-    if (!response.ok) {
-      const responseData = await response.json();
-      let errorMessage = 'Unable to submit profile update request.';
-
-      if (responseData.errors && typeof responseData.errors === 'object') {
-        const firstErrorField = Object.keys(responseData.errors)[0];
-        errorMessage = responseData.errors[firstErrorField][0];
-      }
-      throw new Error(errorMessage);
+    const responseData = await onSubmit();
+    await auth.setUser();
+    if (responseData?.data?.email_update) {
+      props.value.email_update = responseData?.data?.email_update;
     } else {
-      const responseData = await response.json();
-      message.value = responseData.message;
-      success.value = true;
-      await auth.setUser();
-
-      // Update verification status if available
-      if (responseData?.data?.email_update) {
-        props.value.email_update = responseData?.data?.email_update;
-      } else {
-        props.value.email_update = null;
-      }
-    }
-  } catch (err) {
-    message.value = err.message;
-    error.value = true;
-  } finally {
-    loading.value = false;
-  }
-}
-
-const resendVerification = async () => {
-  const config = useRuntimeConfig();
-  error.value = false;
-  success.value = false;
-  message.value = null;
-  loading.value = true;
-
-  try {
-    const response = await fetch(`${config.public.apiBaseUrl}/api/account/email/verification-notification`, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      credentials: 'include',
-      body: JSON.stringify({
-        id: props.value.id,
-        email: props.value.email_update.email,
-      }),
-    });
-
-    if (!response.ok) {
-      const responseData = await response.json();
-      let errorMessage = 'Unable to submit the request to resend the email verification.';
-
-      if (responseData.errors && typeof responseData.errors === 'object') {
-        const firstErrorField = Object.keys(responseData.errors)[0];
-        errorMessage = responseData.errors[firstErrorField][0];
-      }
-      throw new Error(errorMessage);
-    } else {
-      const responseData = await response.json();
-      message.value = responseData.message;
-      success.value = true;
-    }
-  } catch (err) {
-    message.value = err.message;
-    error.value = true;
-  } finally {
-    loading.value = false;
-  }
-}
-
-const cancelEmailUpdate = async () => {
-  const config = useRuntimeConfig();
-  error.value = false;
-  success.value = false;
-  message.value = null;
-  loading.value = true;
-
-  try {
-    const response = await fetch(`${config.public.apiBaseUrl}/api/account/email/verification-notification`, {
-      method: 'DELETE',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      credentials: 'include',
-      body: JSON.stringify({
-        id: props.value.id,
-        email: props.value.email_update.email,
-      }),
-    });
-
-    if (!response.ok) {
-      const responseData = await response.json();
-      let errorMessage = 'Unable to submit the request to cancel the email change.';
-
-      if (responseData.errors && typeof responseData.errors === 'object') {
-        const firstErrorField = Object.keys(responseData.errors)[0];
-        errorMessage = responseData.errors[firstErrorField][0];
-      }
-      throw new Error(errorMessage);
-    } else {
-      const responseData = await response.json();
-      message.value = responseData.message;
-      success.value = true;
-
       props.value.email_update = null;
     }
   } catch (err) {
+    console.error('Submission error:', err);
+  }
+};
+
+const resendVerification = async () => {
+  error.value = false;
+  success.value = false;
+  message.value = null;
+  loading.value = true;
+
+  try {
+    const responseData = await apiRequest(`${useRuntimeConfig().public.apiBaseUrl}/api/account/email/verification-notification`, 'POST', {
+      id: props.value.id,
+      email: props.value.email_update.email,
+    });
+
+    message.value = responseData.message;
+    success.value = true;
+  } catch (err) {
     message.value = err.message;
     error.value = true;
   } finally {
     loading.value = false;
   }
-}
+};
+
+const cancelEmailUpdate = async () => {
+  error.value = false;
+  success.value = false;
+  message.value = null;
+  loading.value = true;
+
+  try {
+    const responseData = await apiRequest(`${useRuntimeConfig().public.apiBaseUrl}/api/account/email/verification-notification`, 'DELETE', {
+      id: props.value.id,
+      email: props.value.email_update.email,
+    });
+
+    message.value = responseData.message;
+    success.value = true;
+    props.value.email_update = null;
+  } catch (err) {
+    message.value = err.message;
+    error.value = true;
+  } finally {
+    loading.value = false;
+  }
+};
 </script>
 
 <template>
@@ -196,7 +100,7 @@ const cancelEmailUpdate = async () => {
       <AlartSuccessMessage v-if="success && !isTooManyAttempts">{{ message }}</AlartSuccessMessage>
       <AlartErrorMessage v-if="error && !isTooManyAttempts">{{ message }}</AlartErrorMessage>
 
-      <form v-if="!isTooManyAttempts" class="mt-6 space-y-6" @submit.prevent="onSubmit">
+      <form v-if="!isTooManyAttempts" class="mt-6 space-y-6" @submit.prevent="handleSubmitProfileInformation">
         <div>
           <InputLabel for="name" value="Name"/>
 
@@ -237,10 +141,7 @@ const cancelEmailUpdate = async () => {
         </div>
       </form>
 
-      <div v-if="isTooManyAttempts" class="rounded-lg px-10 py-8 w-full max-w-md mx-auto text-left">
-        <FormHeader class="text-left" subTitle="Too many attempts to submit" title="Stop!"/>
-        <FormTooManyAttempt>Please try again later after some time.</FormTooManyAttempt>
-      </div>
+      <FormTooManyAttempt v-if="isTooManyAttempts">Please try again later after some time.</FormTooManyAttempt>
     </template>
   </content-box>
 </template>
